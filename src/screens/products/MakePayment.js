6870e5e2375cd0caea8bdfd84225052ce49bce97
thesaintzion <saintzion777@gmail.com>
2020-12-01 19:49:49 +0100

    added paystack (makepayment page...)

--- src/screens/products/MakePayment.js
+++ src/screens/products/MakePayment.js
@@ -4,24 +4,48 @@ import TextField from '@material-ui/core/TextField';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import IconButton from '@material-ui/core/IconButton';
+import { PaystackButton } from 'react-paystack'
 
 
 
 
  function MakePayment(props) {
 
-  const amount = props.amount / 100;
+  const payStackKey = 'pk_test_35059932bc296eafec72754c75b151d20b6a669c';
+//   const amount = props.amount / 100;
+  const amount = 50000 / 100;
   const [email, setEmail] = React.useState("")
   const [name, setName] =  React.useState("")
   const [phone, setPhone] =  React.useState("")
 
 
-// handle form input change
+  const onSuccess = () =>{
+    alert("Thanks for doing business with us! Come back soon!!")
+  } 
+  
+  const onClose = () => {
+    alert("Wait! Don't leave :(")
+  }
+  
+  const componentProps = {
+    email,
+    amount,
+    metadata: {
+      name,
+      phone,
+    },
+    payStackKey,
+    text: "PROCEED",
+    onSuccess: onSuccess(),
+    onClose:  onClose(),
+  }
+
  
+  
 
 
 React.useEffect(() => {
-
+console.log(componentProps)
 }, [])
 
 
@@ -30,8 +54,8 @@ React.useEffect(() => {
         <IconButton  onClick={props.cancelPayment} edge="start" color="inherit" aria-label="close">
 <ArrowBackIcon />
 </IconButton>
-    <h3>Make Payment</h3>
-
+    <h3>Make Payment <span>{amount}</span></h3>
+<form>
  <TextField
     onChange={ (e) => setName(e.target.value)}
     value={name}
@@ -62,10 +86,11 @@ React.useEffect(() => {
   
     />
 
-  
+  </form>
 
 <div className="text-center mt-5 mb-5 d-flex justify-content-around">
-    <Button className="rounded-60 app-green-bg text-white shadow-sm px-3"><span> Proceed </span> <ArrowForwardIcon/></Button>
+<PaystackButton disabled className="rounded-60 app-green-bg text-white shadow-sm px-5 btn" {...componentProps} />
+    {/* <Button className="rounded-60 app-green-bg text-white shadow-sm px-3"><span> Proceed </span> <ArrowForwardIcon/></Button> */}
 </div>
         </div>
     )
